d67289fb07783f3d1529cd6edc3ede1313c472e9
Miguel <miguelramalho51@gmail.com>
2019-04-20 14:31:15 -0300

    Função update livro Enable

--- src/main/java/digivox/desafio/controller/LivroController.java
+++ src/main/java/digivox/desafio/controller/LivroController.java
@@ -1,13 +1,16 @@
 package digivox.desafio.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import digivox.desafio.model.Livro;
-import digivox.desafio.model.Pessoa;
 import digivox.desafio.repository.LivroRepository;
 
 
@@ -20,15 +23,24 @@ public class LivroController {
 	private LivroRepository livroRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value="/cadastroLivro")
-	public String inicio() {
-		return "cadastro/cadastroLivro";
+	public ModelAndView inicio() {
+		
+		ModelAndView view = new ModelAndView("cadastro/cadastroLivro");
+		view.addObject("livroObj", new Livro());
+		return view;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value="/salvarLivro")
-	public String salvar(Livro livro) {
+	@RequestMapping(method = RequestMethod.POST, value="**/salvarLivro")
+	public ModelAndView salvar(Livro livro) {
 
 		livroRepository.save(livro);
-		return "cadastro/cadastroLivro";
+		
+		ModelAndView view = new ModelAndView("cadastro/cadastroLivro");
+		Iterable<Livro> livroIt = livroRepository.findAll();
+		view.addObject("livros", livroIt);
+		view.addObject("livroObj", new Livro());
+		
+		return view;
 		
 	}
 	
@@ -38,8 +50,21 @@ public class LivroController {
 		ModelAndView view = new ModelAndView("cadastro/cadastroLivro");
 		Iterable<Livro> livroIt = livroRepository.findAll();
 		view.addObject("livros", livroIt);
+		view.addObject("livroObj", new Livro());
+		
 		return view;
 	}
-	
+					
+	//UPDATE
+	@GetMapping("/alugarLivro/{idLivro}")
+	public ModelAndView alugar(@PathVariable("idLivro") Long idLivro) {
+		
+		Optional<Livro> livro = livroRepository.findById(idLivro);
+		
+		ModelAndView view = new ModelAndView("cadastro/cadastroLivro");
+		view.addObject("livroObj", livro.get());
+		
+		return view;
+	}
 
 }
